41006ceb1f8ea8d7e2a00b6ee522d408ceefac0e
phenom_lalitha_vaddi <lalitha.vaddi@phenompeople.com>
2021-05-04 20:32:59 +0530

    assignment-one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
+import { successComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    successComponent
   ],
   imports: [
     BrowserModule
---
+++ src/custom.js
@@ -0,0 +1,7 @@
+function success() {
+    alert("Inside Success function");
+}
+
+function warning() {
+    alert("Inside Warning function");
+}
